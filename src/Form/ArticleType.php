2b22a2e21b47a549f8d82e0adb704862b313229a
Shinanubis <eric_maginot@hotmail.fr>
2020-11-25 13:29:57 +0100

    minor fix

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('author',EntityType::class, array(
+                'class'   => 'App\Entity\User',
+                'choice_label'    => 'id', //attribut we want get for User class
+                'multiple' => false,
+                'label'=> 'author'
+            ));
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
